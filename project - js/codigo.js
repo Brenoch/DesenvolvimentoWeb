0e45f90a6acd341dccdb82754f5268f1a5f66979
BRENO CHAVES DA CUNHA <202402798502@alunos.ibmec.edu.br>
2024-11-08 15:15:48 -0300

    localstorage

--- project - js/codigo.js
+++ project - js/codigo.js
@@ -1,4 +1,29 @@
-const url = "https://botafogo-atletas.mange.li/2024-1/";
+const url = "https://botafogo-atletas.mange.li/2024-1/"
+
+const container = document.getElementById("container");
+
+const manipulaCLick = (e) => {
+    const id = e.currentTarget.dataset.id;
+
+    const url = `atleta.html?id=${id}`;
+
+    // cookie
+    document.cookie = `id=${id}`;
+    document.cookie = `nJogos=${e.currentTarget.dataset.nJogos}`;
+
+    // Local Storage
+    localStorage.setItem('id', id);
+    localStorage.setItem('dados', JSON.stringify(e.currentTarget.dataset));
+
+    //Session Storage
+    sessionStorage.setItem('id', id);
+    sessionStorage.setItem('dados', JSON.stringify(e.currentTarget.dataset));
+
+    window.location.href = url; 
+
+    console.log(e.currentTarget);
+}
+
 
 const pega_json = async (caminho) => {
     const resposta = await fetch(caminho);
@@ -6,34 +31,43 @@ const pega_json = async (caminho) => {
     return dados;
 }
 
-const container = document.getElementById("container");
-
 const montaCard = (atleta) => {
     const cartao = document.createElement("article");
     const nome = document.createElement("h1");
     const imagem = document.createElement("img");
-    const descri = document.createElement("p");
-    const link  = document.createElement("a");
-
-    nome.innerHTML = atleta.nome;
-    nome.style.fontFamily = 'sans-serif';  // Corrigido de 'none' para 'nome'
+    const descricao = document.createElement("p");
+    // const link = document.createElement("a");
+    
+    nome.innerText = atleta.nome;
+    nome.style.fontFamily= 'sains-serif';
     cartao.appendChild(nome);
-
+    
     imagem.src = atleta.imagem;
     cartao.appendChild(imagem);
+    
+    descricao.innerHTML = atleta.detalhes;
+    cartao.appendChild(descricao);
 
-    descri.innerHTML = atleta.detalhes;
-    cartao.appendChild(descri);  // Corrigido de 'appendnChild' para 'appendChild'
+    // link.innerText = "Ver Mais";
+    // link.href = `atleta.html?id=${atleta.id}`;
+    // cartao.appendChild(link)
 
-
-    link.innerText = 'Saiba Mais...';
-    link.href = `detalhes.html?id=${atleta.id}`;
-    cartao.appendChild(link);
+    cartao.dataset.id = atleta.id;
+    cartao.dataset.nJogos = atleta.n_jogos;
+    
+    cartao.onclick = manipulaCLick;
+    
     return cartao;
-};
+}
 
-pega_json(`${url}feminino`).then((r) => {
-    r.forEach((ele) => container.appendChild(montaCard(ele)));
-});
+pega_json(`${url}feminino`).then(
+    (r) => {
+        r.forEach(
+            (ele) => container.appendChild(montaCard(ele))
+        )       
+    } 
+);
 
-console.log("isso imprime primeiro");
+// pega_json(`${url}26`).then(
+//     (r) => console.log(r)
+// )
